6fa3f129a2c8c02a443eeadcd966c9a3f72669a4
Salih Durak <76474317+78e49c@users.noreply.github.com>
2023-12-08 20:12:18 +0300

    add display related classes

---
+++ OOTransportMaster/src/display/Frame.java
@@ -0,0 +1,29 @@
+package display;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Frame extends JFrame {
+    public Frame(Panel panel) {
+        super("OOCampus");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(panel);
+
+        Dimension panelSize = panel.getPreferredSize();
+        int extraWidth = 14;
+        int extraHeight = 37;
+
+        setSize(1248+extraWidth, 702+extraHeight);
+
+
+        /*pack();*/
+
+       /* Dimension panelSize = panel.getPreferredSize();
+        setSize(panelSize.width + getInsets().left + getInsets().right,
+                panelSize.height + getInsets().top + getInsets().bottom);*/
+
+
+        setVisible(true);
+    }
+}
