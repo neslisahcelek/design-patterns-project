c7a96b3f53363b8473add9dd862e14ecc2a1f931
Salih Durak <76474317+78e49c@users.noreply.github.com>
2023-12-11 17:37:43 +0300

    update passenger

--- OOTransportMaster/src/observer/Passenger.java
+++ OOTransportMaster/src/observer/Passenger.java
@@ -1,8 +1,20 @@
 package observer;
 
 
+import display.Drawable;
+import motion.Movable;
+import motion.Position;
+
 public class Passenger implements Observer {
-    int location;
+    private int location;
+    private Movable movable;
+    private Drawable drawable;
+
+    public Passenger(Movable movable, Drawable drawable) {
+        this.movable = movable;
+        this.drawable = drawable;
+    }
+
     @Override
     public void update() {
         //shuttle start, phone light on
@@ -14,6 +26,24 @@ public class Passenger implements Observer {
 
     public void setLocation(int i) {
         location = i;
+    }
+
+    public Movable getMovable() {
+        return movable;
+    }
+
+    public void setMovable(Movable movable) {
+        this.movable = movable;
+    }
+
+    public Drawable getDrawable() {return drawable;}
+
+    public void setDrawable(Drawable drawable) {this.drawable = drawable;}
+
+    public void updatePosition(Position position)
+    {
+        movable.setPosition(position);
+        drawable.setPosition(position);
     }
 }
 
