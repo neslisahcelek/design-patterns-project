c31d39af1a45664e6886c28da0958bbd9d4db4fa
Neslişah Çelek <neslisah.celek@outlook.com>
2023-12-11 20:21:57 +0300

    make passenger abstract

--- OOTransportMaster/src/observer/IPassenger.java
+++ OOTransportMaster/src/observer/IPassenger.java
@@ -1,6 +1,20 @@
 package observer;
 
-public interface IPassenger {
-    int getLocation();
-    void setLocation(int i);
+import display.drawable.DrawableBehavior;
+
+public abstract class IPassenger {
+    DrawableBehavior drawableBehavior;
+    int station;
+
+    public IPassenger() {}
+
+    public DrawableBehavior getDrawable() {return drawableBehavior;}
+
+    public int getStation() {
+        return station;
+    }
+
+    void setStation(int station) {
+        this.station = station;
+    }
 }
