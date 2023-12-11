13fca15246e514d4b34e57cc7c8e07b1ad94af4d
Salih Durak <76474317+78e49c@users.noreply.github.com>
2023-12-11 17:38:37 +0300

    add velocitydirection

---
+++ OOTransportMaster/src/motion/VelocityDirection.java
@@ -0,0 +1,26 @@
+package motion;
+
+public class VelocityDirection {
+    private double i;
+    private double j;
+    public VelocityDirection(double i, double j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public double getI() {
+        return i;
+    }
+
+    public void setI(double i) {
+        this.i = i;
+    }
+
+    public double getJ() {
+        return j;
+    }
+
+    public void setJ(double j) {
+        this.j = j;
+    }
+}
