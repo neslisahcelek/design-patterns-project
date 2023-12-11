dd4f308c93d8b7b8bb73a205909517f31ff0439f
Salih Durak <76474317+78e49c@users.noreply.github.com>
2023-12-11 17:37:26 +0300

    update shuttle

--- OOTransportMaster/src/observer/Shuttle.java
+++ OOTransportMaster/src/observer/Shuttle.java
@@ -1,12 +1,20 @@
 package observer;
 
+import display.Drawable;
+import motion.Movable;
+import motion.Position;
+
 import java.util.ArrayList;
 
 public class Shuttle implements Subject{ //Receiver
     private ArrayList<Observer> passengers;
+    private Movable movable;
+    private Drawable drawable;
 
-    public Shuttle() {
+    public Shuttle(Movable movable,Drawable drawable) {
         passengers = new ArrayList<>();
+        this.movable = movable;
+        this.drawable = drawable;
     }
 
     @Override
@@ -26,5 +34,23 @@ public class Shuttle implements Subject{ //Receiver
 
     public ArrayList<Observer> getPassengers() {
         return passengers;
+    }
+
+    public Movable getMovable() {
+        return movable;
+    }
+
+    public void setMovable(Movable movable) {
+        this.movable = movable;
+    }
+
+    public Drawable getDrawable() {return drawable;}
+
+    public void setDrawable(Drawable drawable) {this.drawable = drawable;}
+
+    public void updatePosition(Position position)
+    {
+        movable.setPosition(position);
+        drawable.setPosition(position);
     }
 }
