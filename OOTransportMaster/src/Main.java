4ff88dcd05578a9768735d125b8682d18ee25c34
Neslişah Çelek <neslisah.celek@outlook.com>
2023-12-11 18:52:45 +0300

    implement command and add methods

--- OOTransportMaster/src/Main.java
+++ OOTransportMaster/src/Main.java
@@ -1,6 +1,9 @@
 import command.Command;
 import command.ShuttleApp;
 import command.ShuttleCallCommand;
+import display.Drawable;
+import motion.Motion;
+import motion.Movable;
 import observer.Observer;
 import observer.Passenger;
 import observer.Shuttle;
@@ -11,38 +14,71 @@ import shuttlemanager.ShuttleManager;
 import java.util.ArrayList;
 
 public class Main {
+    private static ArrayList<Passenger> passengers = new ArrayList<>();
+    private static ArrayList<Shuttle> shuttles = new ArrayList<>();
+    private static ShuttleApp shuttleApp = new ShuttleApp();
+
     public static void main(String[] args) {
-        ShuttleApp shuttleApp = new ShuttleApp();
+        addShuttle(1,1);
+        addPassenger(2,3);
+        addPassenger(2,3);
+        addPassenger(2,3);
+        addPassenger(2,3);
 
-        Shuttle shuttle = new Shuttle();
-        Passenger passenger = new Passenger();
-        Passenger passenger2 = new Passenger();
-        Passenger passenger3 = new Passenger();
+        for (Passenger p : passengers) {
+            callCommand(shuttles.get(0), p);
+        }
 
-        Command shuttleCallCommand = new ShuttleCallCommand(shuttle, passenger);
-        Command shuttleCallCommand2 = new ShuttleCallCommand(shuttle, passenger2);
-        Command shuttleCallCommand3 = new ShuttleCallCommand(shuttle, passenger3);
-
-        shuttleApp.setCommand(shuttleCallCommand);
-        shuttleApp.setCommand(shuttleCallCommand2);
-        shuttleApp.setCommand(shuttleCallCommand3);
-
-        shuttleApp.takeCommandCall(shuttleCallCommand);
-        shuttleApp.takeCommandCall(shuttleCallCommand2);
-        shuttleApp.takeCommandCall(shuttleCallCommand3);
-
-        passenger.setLocation(15);
-        passenger2.setLocation(5);
-        passenger3.setLocation(8);
+        int i = 3;
+        for (Passenger p : passengers) {
+            p.setLocation(++i);
+        }
 
         // start journey
-        ShuttleManager sm = new ShuttleManager(shuttle);
+        ShuttleManager sm = new ShuttleManager(shuttles.get(0));
         Route[][] route = Chart.getChart();
         ArrayList<Passenger> passengers = new ArrayList<>();
-        for (Observer p : shuttle.getPassengers()) {
+        for (Observer p : shuttles.get(0).getPassengers()) {
             passengers.add((Passenger) p);
         }
         sm.findShortestRoute(passengers, 1, route);
 
     }
+
+    static void addShuttle(int positionI, int positionJ)
+    {
+        int shuttleImage = 3;// shuttle image code
+
+        Shuttle shuttle = new Shuttle(new Movable(positionI,positionJ),new Drawable(positionI,positionJ,shuttleImage));
+        shuttles.add(shuttle);
+        
+        shuttle.getMovable().setVelocityDirection(Motion.calculateVelocityDirection(shuttle.getMovable(),5,5));
+        shuttle.getMovable().setSpeed(5);
+        System.out.println( shuttle.getMovable().getPosition().getI() + " " +  shuttle.getMovable().getPosition().getJ());
+        shuttle.updatePosition(Motion.calculateNextPositionWithDestination( shuttle.getMovable(),20,20));
+
+    }
+
+    static void addPassenger(int positionI, int positionJ)
+    {
+        int passengerImage = 2;// passenger image code
+
+        Passenger passenger = new Passenger(new Movable(positionI,positionJ),new Drawable(positionI,positionJ,passengerImage));
+        passengers.add(passenger);
+    }
+
+    static void callCommand(Shuttle shuttle, Passenger passenger) {
+        Command shuttleCallCommand = new ShuttleCallCommand(shuttle, passenger);
+        setCommand(shuttleCallCommand);
+    }
+
+    static void setCommand(Command command) {
+        shuttleApp.setCommand(command);
+        takeCommandCall(command);
+    }
+
+    static void takeCommandCall(Command command) {
+        shuttleApp.takeCommandCall(command);
+    }
+
 }
