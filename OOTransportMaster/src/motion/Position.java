322d42ee60186a247db3bea8989214746a0508a6
Salih Durak <76474317+78e49c@users.noreply.github.com>
2023-12-11 17:38:05 +0300

    add position

---
+++ OOTransportMaster/src/motion/Position.java
@@ -0,0 +1,27 @@
+package motion;
+
+public class Position {
+    public Position(double i, double j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    private double i;
+    private double j;
+
+    public double getI() {
+        return i;
+    }
+
+    public void setI(double i) {
+        this.i = i;
+    }
+
+    public double getJ() {
+        return j;
+    }
+
+    public void setJ(double j) {
+        this.j = j;
+    }
+}
