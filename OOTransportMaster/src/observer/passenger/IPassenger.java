70bff6f2c6d48fe7b835a635fd7c4c10833477b8
Neslişah Çelek <neslisah.celek@outlook.com>
2023-12-13 14:22:48 +0300

    add movable behavior to passenger

--- OOTransportMaster/src/observer/passenger/IPassenger.java
+++ OOTransportMaster/src/observer/passenger/IPassenger.java
@@ -1,18 +1,22 @@
 package observer.passenger;
 
 import display.drawable.DrawableBehavior;
-import motion.Position;
+import movement.Position;
+import movement.movable.MovableBehavior;
 
 public abstract class IPassenger {
     DrawableBehavior drawable;
+    MovableBehavior movable;
     int station;
 
     public IPassenger() {}
 
     public DrawableBehavior getDrawable() {return drawable;}
+    public MovableBehavior getMovable() {return movable;}
 
     public void updatePosition(Position position)
     {
+        movable.setPosition(position);
         drawable.setPosition(position);
     }
 
