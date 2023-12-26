b55612fd524e17d3d549eb5c66ac7917ce463a4c
Neslişah Çelek <neslisah.celek@outlook.com>
2023-12-26 20:56:53 +0300

    get images with url

--- OOTransportMaster/src/display/Image.java
+++ OOTransportMaster/src/display/Image.java
@@ -1,10 +1,13 @@
 package display;
 
+import display.drawable.util.ImageUrl;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class Image
 {
@@ -22,72 +25,89 @@ public class Image
     private static Color[][] shuttle;
     private static Color[][] mirroredPassenger;
     private static Color[][] mirroredShuttle;
-    private static final String filePath = Common.filePath;
 
 
     public Color[][] getMap() {
         if (map == null) {
-            map = createImage("map.png");
+            map = PNGtoColorArray(ImageUrl.getMapUrl());
         }
         return map;
     }
     public Color[][] getPassenger() {
         if (passenger == null) {
-            passenger = createImage("passenger.png");
+            passenger = PNGtoColorArray(ImageUrl.getPassengerUrl());
         }
         return passenger;
     }
     public Color[][] getShuttle() {
         if (shuttle == null) {
-            shuttle = createImage("shuttle.png");
+            shuttle = PNGtoColorArray(ImageUrl.getShuttleUrl());
         }
         return shuttle;
     }
     public Color[][] getMirroredPassenger() {
         if (mirroredPassenger == null) {
-            mirroredPassenger = createImage("passenger.png");
+            mirroredPassenger = PNGtoColorArray(ImageUrl.getPassengerUrl());
             mirroredPassenger = Display.mirror(mirroredPassenger);
         }
         return mirroredPassenger;
     }
     public Color[][] getMirroredShuttle() {
         if (mirroredShuttle == null) {
-            mirroredShuttle = createImage("shuttle.png");
+            mirroredShuttle = PNGtoColorArray(ImageUrl.getShuttleUrl());
             mirroredShuttle = Display.mirror(mirroredShuttle);
         }
         return mirroredShuttle;
     }
 
-    private static Color[][] createImage(String path) {
-        return PNGtoColorArray(filePath+path);
-    }
-    private static Color[][] PNGtoColorArray(String filePath) {
-
-        File file = new File(filePath);
-        BufferedImage image;
-
+    private static Color[][] PNGtoColorArray(String url) {
         try {
-            image = ImageIO.read(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            URL imageUrl = new URL(url);
+            BufferedImage image;
+            image = ImageIO.read(imageUrl);
+            int width = image.getWidth();
+            int height = image.getHeight();
 
-        int width = image.getWidth();
-        int height = image.getHeight();
+            Color[][] colors = new Color[height][width];
 
-        Color[][] colors = new Color[height][width];
-
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int rgb = image.getRGB(x,y);
-                colors[y][x] = new Color(rgb);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int rgb = image.getRGB(x,y);
+                    colors[y][x] = new Color(rgb);
+                }
             }
+
+            return colors;
+
+        } catch (IOException e) {
+            System.out.println("Images can not loaded.");
+            return null;
         }
+    }
 
-        return colors;
+    private static Color[][] PNGtoColorArrayWithFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            BufferedImage image;
+            image = ImageIO.read(file);
+            int width = image.getWidth();
+            int height = image.getHeight();
 
+            Color[][] colors = new Color[height][width];
 
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int rgb = image.getRGB(x,y);
+                    colors[y][x] = new Color(rgb);
+                }
+            }
+
+            return colors;
+
+        } catch (IOException e) {
+            System.out.println("Images can not loaded.");
+            return null;
+        }
     }
 }
 
