760d889faa3f99b7925dec7ffc3eee3abff07a7a
Salih Durak <76474317+78e49c@users.noreply.github.com>
2023-12-26 17:24:36 +0300

    Update DrawableShuttle.java

--- OOTransportMaster/src/display/drawable/DrawableShuttle.java
+++ OOTransportMaster/src/display/drawable/DrawableShuttle.java
@@ -3,9 +3,12 @@ package display.drawable;
 import movement.Position;
 
 public class DrawableShuttle extends DrawableBehavior {
+
     public DrawableShuttle(double positionI, double positionJ) {
         super.position = new Position(positionI,positionJ);
         super.type = 3;
+        super.widthShift = -50;
+        super.lengthShift = -17;
     }
 }
 
