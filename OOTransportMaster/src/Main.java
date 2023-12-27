913380daeb373f95d0f01c2b12c5eea008772c7d
Neslişah Çelek <neslisah.celek@outlook.com>
2023-12-27 20:02:43 +0300

    add test for singleton pattern

--- OOTransportMaster/src/Main.java
+++ OOTransportMaster/src/Main.java
@@ -2,15 +2,23 @@
 //import display.Drawable;
 //import display.drawable.DrawableShuttleBehavior;
 //import motion.movable.MovableShuttle;
-
-
+import display.Image;
 import manager.Process;
 
 public class Main {
-
-
     public static void main(String[] args) {
+        testSingleton();
         Process.display();
+    }
+
+    public static void testSingleton() {  //Singleton Pattern
+        Image image1 = Image.getImage();
+        Image image2 = Image.getImage();
+        if (image1 == image2) {
+            System.out.println("Image1 and Image2 reference the same object. Image class is Singleton!");
+        } else {
+            System.out.println("Image1 and Image2 reference different objects.");
+        }
     }
 
 
